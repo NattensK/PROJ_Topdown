a76ac871816fb067c47df0fa5957fd8e4901fc73
Rufus Trukhin <rufustrukhin@gmail.com>
2024-10-22 17:01:43 +0200

    Coding (Rider) Environment Setup

---
+++ Source/Untitled_Topdown/Untitled_Topdown.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Untitled_Topdown.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Untitled_Topdown, "Untitled_Topdown" );
